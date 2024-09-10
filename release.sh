12883ede2ba39fffbe6225ab8351acb05d673a39
N-Storm <N-Storm@users.noreply.github.com>
2024-09-10 03:35:17 +0300

    Added CGO + NOCGO builds

--- release.sh
+++ release.sh
@@ -21,6 +21,12 @@ echo "Compiling:"
 os="windows linux"
 arch="amd64 arm arm64"
 pushd cmd/ck-client
+CGO_ENABLED=0 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
+strip ck-client-linux-amd64*
+aarch64-linux-gnu-strip ck-client-linux-arm64*
+arm-linux-gnueabihf-strip ck-client-linux-arm-*
+for file in ck-client-*; do mv ${file} ${file}-nocgo ; done
+
 CGO_ENABLED=1 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
 strip ck-client-linux-amd64*
 aarch64-linux-gnu-strip ck-client-linux-arm64*
@@ -31,6 +37,12 @@ popd
 os="linux"
 arch="amd64 arm arm64"
 pushd cmd/ck-server
+CGO_ENABLED=1 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
+strip ck-server-linux-amd64*
+aarch64-linux-gnu-strip ck-server-linux-arm64*
+arm-linux-gnueabihf-strip ck-server-linux-arm-*
+for file in ck-server-*; do mv ${file} ${file}-nocgo ; done
+
 CGO_ENABLED=1 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
 strip ck-server-linux-amd64*
 aarch64-linux-gnu-strip ck-server-linux-arm64*
