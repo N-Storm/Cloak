6d5c13e6d88b1c83407ea277a5029148940c2c39
N-Storm <N-Storm@users.noreply.github.com>
2024-12-08 18:06:42 +0300

    Cut down build variants to useful only

--- release.sh
+++ release.sh
@@ -25,29 +25,33 @@ CGO_ENABLED=0 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarc
 strip ck-client-linux-amd64*
 aarch64-linux-gnu-strip ck-client-linux-arm64*
 arm-linux-gnueabihf-strip ck-client-linux-arm-*
-for file in ck-client-*; do newname="$(echo ${file} | sed -E 's/(.exe)?$/-noCGO\1/')"; mv ${file} ../../release/${newname} ; done
-
-CGO_ENABLED=1 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
-strip ck-client-linux-amd64*
-aarch64-linux-gnu-strip ck-client-linux-arm64*
-arm-linux-gnueabihf-strip ck-client-linux-arm-*
+# for file in ck-client-*; do newname="$(echo ${file} | sed -E 's/(.exe)?$/-noCGO\1/')"; mv ${file} ../../release/${newname} ; done
 mv ck-client-* ../../release
+
+CGO_ENABLED=1 gox -ldflags "-X main.version=${v}" -os="$os" -arch="amd64" -osarch="$osarch" -output="$output"
+strip ck-client-linux-amd64*
+# aarch64-linux-gnu-strip ck-client-linux-arm64*
+# arm-linux-gnueabihf-strip ck-client-linux-arm-*
+for file in ck-client-*; do newname="$(echo ${file} | sed -E 's/(.exe)?$/-CGO\1/')"; mv ${file} ../../release/${newname} ; done
+# mv ck-client-* ../../release
 popd
 
 os="linux"
 arch="amd64 arm arm64"
 pushd cmd/ck-server
-CGO_ENABLED=1 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
+CGO_ENABLED=0 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
 strip ck-server-linux-amd64*
 aarch64-linux-gnu-strip ck-server-linux-arm64*
 arm-linux-gnueabihf-strip ck-server-linux-arm-*
-for file in ck-server-*; do newname="$(echo ${file} | sed -E 's/(.exe)?$/-noCGO\1/')"; mv ${file} ../../release/${newname} ; done
-
-CGO_ENABLED=1 gox -ldflags "-X main.version=${v}" -os="$os" -arch="$arch" -osarch="$osarch" -output="$output"
-strip ck-server-linux-amd64*
-aarch64-linux-gnu-strip ck-server-linux-arm64*
-arm-linux-gnueabihf-strip ck-server-linux-arm-*
+# for file in ck-server-*; do newname="$(echo ${file} | sed -E 's/(.exe)?$/-noCGO\1/')"; mv ${file} ../../release/${newname} ; done
 mv ck-server-* ../../release
+
+CGO_ENABLED=1 gox -ldflags "-X main.version=${v}" -os="$os" -arch="amd64" -osarch="$osarch" -output="$output"
+strip ck-server-linux-amd64*
+# aarch64-linux-gnu-strip ck-server-linux-arm64*
+# arm-linux-gnueabihf-strip ck-server-linux-arm-*
+for file in ck-server-*; do newname="$(echo ${file} | sed -E 's/(.exe)?$/-CGO\1/')"; mv ${file} ../../release/${newname} ; done
+# mv ck-server-* ../../release
 popd
 
 sha256sum release/*
